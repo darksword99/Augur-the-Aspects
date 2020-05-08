056dd80026ba58640093d442a0495e1fe52c8ac4
Medow3 <matthew.dawsonhome@gmail.com>
2020-05-07 21:13:32 -0700

    pathing working

--- src/battle/ai/moving.gd
+++ src/battle/ai/moving.gd
@@ -4,13 +4,37 @@ extends Node
 # Declare member variables here. Examples:
 # var a: int = 2
 # var b: String = "text"
+var rng = RandomNumberGenerator.new()
 
+onready var path_follow = $"../../../Path2D/PathFollow2D"
+onready var path_2d = $"../../../Path2D"
+onready var paths = [
+	"../../../left_path", "../../../slight_left_path", "../../../stright_path",
+	"../../../slight_right_path", "../../../right_path"
+]
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	var next_curve = Curve2D.new()
+	next_curve.add_point(Vector2.ZERO, Vector2.ZERO, Vector2(1,1))
+	var x = rng.randi_range(0,4)
+	print(x)
+	next_curve.add_point(Vector2.ZERO + get_node(paths[x]).position, Vector2(1,1))
+	path_2d.set_curve(next_curve)
 
 
 
-func physics() -> void:
-	pass
+func physics(scorpion) -> String:
+	path_follow.offset += 1
+	scorpion.position = path_follow.position
+	scorpion.rotation = path_follow.rotation
+	if path_follow.unit_offset == 1:
+		path_follow.unit_offset = 0
+		rng.randomize()
+		var next_curve = Curve2D.new()
+		next_curve.add_point(scorpion.position, Vector2(10,0), Vector2(10,0))
+		var x = rng.randi_range(0,4)
+		print(x)
+		next_curve.add_point(scorpion.position + get_node(paths[x]).position)
+		path_2d.set_curve(next_curve)
+	return "moving"
--- src/battle/ai/scorpion.gd
+++ src/battle/ai/scorpion.gd
@@ -4,28 +4,28 @@ extends KinematicBody2D
 var movement = Vector2.ZERO
 var speed = 50
 var facing = Vector2(1,1)
-var current_state = "aboveground"
+var current_state = "moving"
 var handling = PI/64
 var next_angle = 0
 
 var rng = RandomNumberGenerator.new()
 onready var navigation = $Navigation2D
 onready var player = $"../../../player" # Instance of relying on things outside of current scene
-
+onready var path_follow = $"../p"
+onready var states_map = {
+	"charging": $states/underground/charging,
+	"moving": $states/underground/moving,
+	"poke": $states/aboveground/attacking/poke,
+	"beam": $states/aboveground/attacking/beam,
+}
 
 func _ready() -> void:
-	rng.randomize()
-	print(rng.randi_range(1,10))
+	pass
 
 
 func _process(delta: float) -> void:
-	pass
-	
-#	movement = Vector2(1, tan(facing.angle() + handling))
-	
-	facing = movement.normalized()
-	movement = movement.normalized() * speed
-	move_and_slide(movement)
+	var new_state = states_map[current_state].physics(self)
+	change_state(new_state)
 
 
 func change_state(new_state: String) -> void:

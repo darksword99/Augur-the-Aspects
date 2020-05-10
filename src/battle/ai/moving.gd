38fc11cc64750b270d6bc12288f37cbba0a1621c
Medow3 <matthew.dawsonhome@gmail.com>
2020-05-10 12:39:48 -0700

    Fixed pathing straight bug.

--- src/battle/ai/moving.gd
+++ src/battle/ai/moving.gd
@@ -9,17 +9,17 @@ var rng = RandomNumberGenerator.new()
 onready var path_follow = $"../../../Path2D/PathFollow2D"
 onready var path_2d = $"../../../Path2D"
 onready var paths = [
-	"../../../left_path", "../../../slight_left_path", "../../../stright_path",
+	"../../../left_path", "../../../slight_left_path", "../../../straight_path",
 	"../../../slight_right_path", "../../../right_path"
 ]
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	var next_curve = Curve2D.new()
-	next_curve.add_point(Vector2.ZERO, Vector2.ZERO, Vector2(1,1))
+	next_curve.add_point(Vector2.ZERO)
 	var x = rng.randi_range(0,4)
 	print(x)
-	next_curve.add_point(Vector2.ZERO + get_node(paths[x]).position, Vector2(1,1))
+	next_curve.add_point(Vector2.ZERO + get_node(paths[x]).position)
 	path_2d.set_curve(next_curve)
 
 
@@ -32,7 +32,7 @@ func physics(scorpion) -> String:
 		path_follow.unit_offset = 0
 		rng.randomize()
 		var next_curve = Curve2D.new()
-		next_curve.add_point(scorpion.position, Vector2(10,0), Vector2(10,0))
+		next_curve.add_point(scorpion.position)
 		var x = rng.randi_range(0,4)
 		print(x)
 		next_curve.add_point(scorpion.position + get_node(paths[x]).position)
